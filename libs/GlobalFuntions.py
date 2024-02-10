96dd14c2fa3574b6569b41fe9cceaf0af9d56199
Viren Tanna <viren121@gmail.com>
2024-02-10 15:56:36 -0500

    GlobalFuntions.py (Greeting & Sum)

---
+++ libs/GlobalFuntions.py
@@ -0,0 +1,16 @@
+def sum(a, b):
+    """
+    sums up two numberics
+    :param a: number
+    :param b: number
+    :return: sum of numbers
+    """
+    return a + b
+
+def greet(name):
+    """
+    Greeting function
+    :param name: String Name
+    :return: greeting message
+    """
+    return "Welcome " + name
