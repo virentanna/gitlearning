50893b8f6f9d8d9003278a2f45855f0c779c1fcf
Viren Tanna <viren121@gmail.com>
2024-02-10 16:13:29 -0500

    Created CR01.py

---
+++ src/CR01.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, "C:\\Users\\viren\\OneDrive\\Documents\\GitHub\\gitLearning")
+from libs.GlobalFuntions import *
+
+if __name__ == '__main__':
+    print(greet("Viren"))
